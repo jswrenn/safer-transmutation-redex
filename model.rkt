12a39aba7dc1670b5c86a6c428515dda75edb829
Jack Wrenn <me@jswrenn.com>
2020-09-17 18:49:09 -0400

    rewrite!

--- model.rkt
+++ model.rkt
@@ -1,157 +1,106 @@
 #lang racket
-
 (require redex)
-
-
 
 (define-language
   layouts
 
-  ; a layout
-  [l :: l+ l× l⋅]
-  
-  [l+ ::= (+ l ...)] ; enum layout
-  [l× ::= (× l ...)] ; struct layout
-  [l⋅ ::= i u] ; atom layout
+  [l ::=
+     ; unions
+     l+
 
-  ; initialized byte(s) in value range
-  [i ::=
-     (side-condition
-      (init (name len natural)
-            (name min natural)
-            (name max natural))
-      (< -1
-         (term min)
-         (term max)
-         (expt 255 (term len))))]
+     ; structs
+     l×
 
-  ; a type
-  ; need to update this for the new `i` :/
-  [t ::=
-   (struct t ...)
-   (union t ...)
-   (array t natural)
-   init
-   foo]
+     ; atoms
+     la]
+
+  [l+ ::= (+ l l)]
+  [l× ::= (× l l)]
+  [la ::=
+      ; zst
+      ε
+
+      ; void
+      ∅
+      
+      ; uninit bytes
+      (u (name len natural))
+      
+      ; init bytes with valid range
+      (side-condition
+       (i (name len natural)
+          (name min natural)
+          (name max natural))
+       (< -1
+          (term min)
+          (term max)
+          (expt 255 (term len))))]
   )
 
-; enum syntactic sugar
-; enum desugars to union of structs
-#;
+; add two numbers
 (define-metafunction layouts
-  enum : (init_!_ (t ...)) ... -> t
-  [(enum (init (t ...)) ...)
-   (union (struct init t ...) ...)])
+  ⊕ : natural natural -> natural
+  [(⊕ natural_0 natural_1)
+   ,(+ (term natural_0) (term natural_1))])
 
+; saturating sub
 (define-metafunction layouts
-  u8= : (side-condition (name value natural)
-                        (<= 0 (term value) 255)) -> l⋅
-  [(u8= natural)
-   (init 1 natural ,(+ (term natural) 1))])
+  ⊖ : natural natural -> natural
+  [(⊖ natural_0 natural_1)
+   ,(min 0 (- (term natural_0) (term natural_1)))])
 
-; align-of
+; append
 (define-metafunction layouts
-  align-of : t -> natural
-  [(align-of foo)
-   2]
-  [(align-of init)
-   1]
-  [(align-of (array t natural))
-   (align-of t)]
-  [(align-of (struct t ...))
-   ,(apply max (term ((align-of t) ...)))]
-  [(align-of (union t ...))
-   ,(apply max (term ((align-of t) ...)))])
+  ⧺ : l l -> l
 
-; size-of
-(define-metafunction layouts
-  size-of : t -> natural
-  [(size-of init)
-   1]
-  [(size-of foo)
-   2]
-  [(size-of (array t natural))
-   ,(* (term (size-of t))
-       (term natural))]
-  [(size-of (struct t ...))
-   ,(apply + (term ((size-of t) ...)))]
-  [(size-of (union t ...))
-   ,(apply max (term ((size-of t) ...)))])
+  ; (⧺ × l)
+  [(⧺ (× l_0 l_1) l_2)
+   (× l_0 (⧺ l_1 l_2))]
 
-(define (round-up-to-multiple offset align)
-  (-
-   (- (+ offset align) 1)
-   (remainder (- (+ offset align) 1) align)
-   offset))
+  ; (⧺ + l)
+  [(⧺ (+ l_0 l_1) l_2)
+   (+ (⧺ l_0 l_2)
+      (⧺ l_1 l_2))]
 
+  ; (⧺ a l)
+  [(⧺ la l)
+   (× la l)]
+  )
 
-(define-metafunction layouts
-  layout-struct : natural number t ... -> l
-  ; trailing padding
-  [(layout-struct (name align natural) (name offset number))
-   (+ ,@(make-list (round-up-to-multiple (term offset) (term align))
-                   (term uninit)))]
-  ; padding + field
-  [(layout-struct (name struct-align natural) (name offset number) t t_rest ...)
-   ,(let*
-        ([align (term (align-of t))]
-         [layout (term (layout-of t))]
-         [offset (term offset)]
-         [padding (round-up-to-multiple offset align)])
-      (term
-       (+ ,@(make-list padding (term uninit))
-          ,layout
-          (layout-struct
-           struct-align
-           ,(+ padding (term (size-of t)))
-           t_rest ...))))])
-
-(define-metafunction layouts
-  layout-of : t -> l
-  [(layout-of init)
-   init]
-  [(layout-of foo)
-   42]
-  [(layout-of (struct t ...))
-   ,(let ([align (term (align-of (struct t ...)))])
-      (term (layout-struct ,align 0 t ...)))])
-
-; algebraic transformation of a term
+; simplify a term
 (define-judgment-form layouts
   #:mode (~ I O)
   #:contract (~ l l)
 
-  ; TODO more forms!
-
-  ; right-associate +
-  [(~ (+ l_0 l_1 l_2 l_n ...)
-      (+ l_0 (+ l_1 l_2 l_n ...)))
-   right-associate-+]
-
-  ; right-associate ×
-  [(~ (× l_0 l_1 l_2 l_n ...)
-      (× l_0 (× l_1 l_2 l_n ...)))
-   right-associate-×]
-
-  ; left-distribute
-  [(~ (× l_m (+ l_a ...))
-      (+ (× l_a l_m) ...))
-   left-distribute]
+  ; reassociate
+  [(~ (× l_0 l_1)
+      (⧺ l_0 l_1))
+   reassociate]
   
+  ; left-distribute
+  [(~ (× l_m (+ l_a0 l_a1))
+      (+ (× l_m l_a0) (× l_m l_a1)))
+   left-distribute]
+
   ; right-distribute
-  [(~ (× (+ l_a ...) l_m)
-      (+ (× l_a l_m) ...))
+  [(~ (× (+ l_a0 l_a1) l_m)
+      (+ (× l_a0 l_m) (× l_a1 l_m)))
    right-distribute]
 
-  ; unwrap lone l from ×
-  [(~ (× l)
-      l)
-   unwrap-×]
+  ; eliminate ε from ×
+  [(~ (× l ε) l) unwrap-×-l]
+  [(~ (× ε l) l) unwrap-×-r]
 
-  ; unwrap lone l from +
-  [(~ (+ l)
-      l)
-   unwrap-+]
+  ; eliminate empty bytes
+  ; u~ε
+  [(~ (u 0)
+      ε)
+   u~ε]
+  ; i~ε
+  [(~ (i 0 _ _)
+      ε)
+   i~ε]
+
   )
 
 ; is a layout transmutable to another layout?
@@ -160,7 +109,6 @@
   #:contract (→ l l)
 
   ; algebraic transformations of src and dst
-  
   [ (~ l_src l_src′) (→ l_src′ l_dst)
    ----------------------------------- src′
             (→ l_src l_dst)          ]
@@ -170,97 +118,148 @@
             (→ l_src l_dst)          ]
 
   
+  ;; +→∗
+  ; 1. +→×
+  ; 2. +→a
+  ; 3. +→+
+
   ; every variant of the src must be transmutable to dst
-  [  (→ l_src l_dst) ...
-   ------------------------ +→∗
-   (→ (+ l_src ...) l_dst)]
+  [(→ l_src_0 l_dst) (→ l_src_1 l_dst)
+   ----------------------------------- +→∗
+     (→ (+ l_src_0 l_src_1) l_dst)   ]
 
-  ; there must exist a variant of the dst such that the src
-  ; is transmutable to that variant
-  [        (→ l_src l_dst)
-   -------------------------------- ∗→+
-   (→ l_src (+ _ ... l_dst _ ...))]
+  
+  ;; ∗→+
+  ; 3. +→+
+  ; 4. a→+
+  ; 5. ×→+
 
+  ; src is transmutable to the left variant of dst
+  [      (→ l_src l_dst_0)
+   ------------------------------ ∗→+-L
+   (→ l_src (+ l_dst_0 l_dst_1))]
 
-  ; × → ×
-
-  ; nom
-  [(→ l_src l_dst) (→ (× l_src_n ...) (× l_dst_n ...))
-   ---------------------------------------------------- +→+:nom
-     (→ (× l_src l_src_n ...) (× l_dst l_dst_n ...))  ]
-
-  ; bytes may be truncated from the `dst`; e.g.:
-  ; union Transmute { src: u8, dst: () }
-  [------------------- ×→×:shrink
-   (→ (× l ...) (×)) ]
-
-  ; uninit bytes may be conjure from the `src`; e.g.: 
-  ; union Transmute { src: (), dst: u8 }
-  [------------------------ ×→×:grow
-   (→ (×) (× u ...)) ]
+  ; src is transmutable to the right variant of dst
+  [      (→ l_src l_dst_1)
+   ------------------------------ ∗→+-R
+   (→ l_src (+ l_dst_0 l_dst_1))]
 
 
-  ; ⋅ → ⋅
-  [-------- init→init
-   (→ i i)]
+  
+  ;; 6. ×→×
+  ; the cases where the first operand of ×
+  ; isn't an atom are handled by ~
+  [     (⇝ la_src la_dst la_src′ la_dst′)
+    (→ (× la_src′ l_src) (× la_dst′ l_dst))
+   ----------------------------------------- ×→×
+     (→ (× la_src l_src) (× la_dst l_dst)) ]
 
-  [-------- init→uninit
-   (→ i u)]
+  ;; 7. ×→a
+  [(⇝ la_src la_dst la_src′ la_dst′)
+     (→ (× la_src′ l_src) la_dst′)
+   --------------------------------- ×→a
+     (→ (× la_src l_src) la_dst)   ]
 
-  [-------- uninit→uninit
-   (→ u u)]
+  ;; 8. a→×
+  [(⇝ la_src la_dst la_src′ la_dst′)
+     (→ la_src′ (× la_dst′ l_dst))
+   --------------------------------- a→×
+     (→ la_src (× la_dst l_dst))   ]
+
+  ;; 9. a→a
+  [(⇝ la_src la_dst la_src′ la_dst′)
+          (→ la_src′ la_dst′)
+   --------------------------------- a→a
+           (→ la_src la_dst)       ]
+  
   )
 
-
-(test-judgment-holds
- (→ (u8= 1) (u8= 1)))
-
-
-; shrink
-(test-judgment-holds
- (→ (× (u8= 1)) (×)))
-
-; grow
-(test-judgment-holds
- (→ (×) (× u)))
-
-#;
-(traces	
- ~
- (term (× (u8= 1) (u8= 2)))
- #:edge-labels? #t
- )
-
-
 ; nom
-(test-judgment-holds
- (→ (× (u8= 1) (u8= 2)) (× (u8= 1) (u8= 2))))
+; ⇝ consumes a src atom and dst atom,
+; chomps as much as possible off src and dst
+; and produces their remainders
+(define-judgment-form layouts
+  #:mode (⇝ I I O O)
+  #:contract (⇝ la la la la)
 
+  ; ∅ is transmutable into everything
+  ; fine since ∅ isn't constructible
+  [(⇝ ∅ l
+      ε l)]
 
+  ; u→u
+  [(where natural_src_len′
+          (⊖ natural_src_len natural_dst_len))
+   (where natural_dst_len′
+          (⊖ natural_dst_len natural_src_len))
+   ---------------------------------------------
+   (⇝ (u natural_src_len) (u natural_dst_len)
+      ; into:
+      (u natural_src_len′) (u natural_dst_len′))]
 
-(test-judgment-holds
- (→ (+ (× (u8= 1))) (+ (× (u8= 1)) (× (u8= 2)) (× (u8= 3)))))
+  ; i→u
+  [(where natural_src_len′
+          (⊖ natural_src_len natural_dst_len))
+   ; we're truncating the src, so the min and max
+   ; bounds must be adjusted accordingly
+   ; this will differ depending on endian
+   (where natural_src_min′
+          #| TODO |# hole)
+   (where natural_src_max′
+          #| TODO |# hole)
+   
+   (where natural_dst_len′
+          (⊖ natural_dst_len natural_src_len))
+   ---------------------------------------------
+   (⇝ (i natural_src_len
+         natural_src_min
+         natural_src_max)
+      (u natural_dst_len)
+      ; into
+      (i natural_src_len′
+         natural_src_min′
+         natural_src_max′)
+      (u natural_dst_len′))]
 
-(test-judgment-holds
- (→ (+ (× (u8= 2))) (+ (× (u8= 1)) (× (u8= 2)) (× (u8= 3)))))
+  ; i→i
+  [(where natural_src_len′
+          (⊖ natural_src_len natural_dst_len))
+   ; we're truncating the src, so the min and max
+   ; bounds must be adjusted accordingly
+   ; this will differ depending on endian
+   (where natural_src_min′
+          #| TODO |# hole)
+   (where natural_src_max′
+          #| TODO |# hole)
+   
+   (where natural_dst_len′
+          (⊖ natural_dst_len natural_src_len))
+   ; we're truncating the dst, so the min and max
+   ; bounds must be adjusted accordingly
+   (where natural_dst_min′
+          #| TODO |# hole)
+   (where natural_dst_max′
+          #| TODO |# hole)
+   ---------------------------------------------
+   (⇝ (i natural_src_len
+         natural_src_min
+         natural_src_max)
+      (i natural_dst_len
+         natural_dst_min
+         natural_dst_max)
+      ; into
+      (i natural_src_len′
+         natural_src_min′
+         natural_src_max′)
+      (i natural_dst_len′
+         natural_dst_min′
+         natural_dst_max′))]
+  
+  )
 
-(test-judgment-holds
- (→ (+ (× (u8= 3))) (+ (× (u8= 1)) (× (u8= 2)) (× (u8= 3)))))
-
-; dist
-(test-judgment-holds
- (→
-  (+ (× (u8= 1) (u8= 3)) (× (u8= 2) (u8= 3)))
-  (× (+ (u8= 1) (u8= 2)) (u8= 3))
- ))
-
-; more dist
-(test-judgment-holds
- (→
-  (× (+ (u8= 1) (u8= 2)) (u8= 3) (u8= 4))
-  (+ (× (u8= 1) (u8= 3) (u8= 4)) (× (u8= 2) (u8= 3) (u8= 4)))
- ))
-
-#||#
-
-(test-results)
+; shorthand for test cases
+(define-metafunction layouts
+  u8 : (side-condition (name value natural)
+                       (<= 0 (term value) 255)) -> l⋅
+  [(u8 natural)
+   (init 1 natural ,(+ (term natural) 1))])
